2cdbb49cd03ddd8eca1a0f73fd041c158f8733cf
lliscano@conexia.com <lliscano@conexia.com>
2023-09-12 14:46:26 -0500

    Fix: Datasource Primary h2

---
+++ src/main/java/com/lliscano/databases/h2/H2DataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.lliscano.databases.h2;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class H2DataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.h2")
+    public DataSourceProperties h2DataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    public DataSource h2DataSource() {
+        return h2DataSourceProperties()
+                .initializeDataSourceBuilder()
+                .build();
+    }
+}
